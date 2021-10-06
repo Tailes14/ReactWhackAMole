bb74f49c76618ea6630b416123bece934db0fea9
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2021-10-05 20:59:35 -0400

    setup headers and grid

--- game/src/App.js
+++ game/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+function Headers({score, bestScore, setBestScore, time}) {
+  return <>
+  <div class="headers">
+    <h1>High Score: {bestScore}</h1>
+    <h1>Current Score: {score}</h1>
+    <h1>Time Remaining: <span id="remainingTime">{time}</span></h1>
+  </div>
+  </>
+}
+
+function PlayGrid({}) {
+
+  return <>
+  <div class="playGrid">
+    <div class="square" id="1"></div>
+    <div class="square" id="2"></div>
+    <div class="square" id="3"></div>
+    <div class="square" id="4"></div>
+    <div class="square" id="5"></div>
+    <div class="square" id="6"></div>
+    <div class="square" id="7"></div>
+    <div class="square" id="8"></div>
+    <div class="square" id="9"></div>
+  </div>
+  </>
+}
+
+function ScoreTable({}) {
+  return <>
+  </>
+}
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [time, setTime] = useState(0);
+
+  const handleStart = (() => {
+    setTime(60)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Headers score={score} bestScore={bestScore} setBestScore={setBestScore} time={time}/>
+      <PlayGrid/>
+      <input id="startGame" type="button" value="Start Game" onClick={handleStart}/>
+      <ScoreTable/>
+
     </div>
   );
 }
