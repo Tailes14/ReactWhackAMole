15c8e19e43165703de6cca35d3b4a8a4869e1d3b
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2021-10-06 21:49:08 -0400

    got random mole pos

--- game/src/App.js
+++ game/src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 
 function Headers({score, bestScore, setBestScore, time}) {
   return <>
-  <div class="headers">
+  <div className="headers">
     <h1>High Score: {bestScore}</h1>
     <h1>Current Score: {score}</h1>
     <h1>Time Remaining: <span id="remainingTime">{time}</span></h1>
@@ -12,19 +12,19 @@ function Headers({score, bestScore, setBestScore, time}) {
   </>
 }
 
-function PlayGrid({}) {
-
+function PlayGrid( {molePoisiton}) {
+ 
   return <>
-  <div class="playGrid">
-    <div class="square" id="1"></div>
-    <div class="square" id="2"></div>
-    <div class="square" id="3"></div>
-    <div class="square" id="4"></div>
-    <div class="square" id="5"></div>
-    <div class="square" id="6"></div>
-    <div class="square" id="7"></div>
-    <div class="square" id="8"></div>
-    <div class="square" id="9"></div>
+  <div className="playGrid">
+    <div className="square" id="1"></div>
+    <div className="square" id="2"></div>
+    <div className="square" id="3"></div>
+    <div className="square" id="4"></div>
+    <div className="square" id="5"></div>
+    <div className="square" id="6"></div>
+    <div className="square" id="7"></div>
+    <div className="square" id="8"></div>
+    <div className="square" id="9"></div>
   </div>
   </>
 }
@@ -34,23 +34,21 @@ function ScoreTable({}) {
   </>
 }
 
-function GameMechanics({setScore, setTime, time}) {
-  let gameTime = time
-  for (let i= 0; i < gameTime; i++) {
-    setTime(time-1)
-  }
-}
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [time, setTime] = useState(5);
   const [toggleStart, setToggleStart] = useState(false)
+  const [molePoisiton, setMolePoisiton] = useState(0)
+
+
 
   useEffect(() => {
     if (toggleStart === true) {
       let gameClock = setTimeout(() => {
         setTime(time-1);
+        setMolePoisiton(Math.floor(Math.random() * 9))
       }, 1000)
       if (time === 0) {
         clearInterval(gameClock)
@@ -61,10 +59,9 @@ function App() {
   return (
     <div className="App">
       <Headers score={score} bestScore={bestScore} setBestScore={setBestScore} time={time}/>
-      <PlayGrid/>
+      <PlayGrid molePoisiton={molePoisiton}/>
       <input id="startGame" type="button" value="Start Game" onClick={() => setToggleStart(true)}/>
       <ScoreTable/>
-
     </div>
   );
 }
